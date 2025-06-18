0a76564295fb9463ff454356af5539ae6cb51e16
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-06-19 01:53:22 +0200

    fix: Correct import order in KinkCategory component

--- src/components/KinkCategory.tsx
+++ src/components/KinkCategory.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react'
 import { useTranslation } from 'react-i18next'
-import KinkRow from './KinkRow'
 import { strToClass } from '../utils'
+import KinkRow from './KinkRow'
 
 interface KinkCategoryProps {
   name: string
